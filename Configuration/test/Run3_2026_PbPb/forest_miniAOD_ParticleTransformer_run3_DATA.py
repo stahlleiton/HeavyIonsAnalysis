28866e108154f962d778bfae5d9d9b43b7e4841d
Andre Govinda Stahl Leiton <andre.govinda.stahl.leiton@cern.ch>
2026-09-08 00:59:02 +0200

    Updated 2026 PbPb setup

--- Configuration/test/Run3_2026_PbPb/forest_miniAOD_ParticleTransformer_run3_DATA.py
+++ Configuration/test/Run3_2026_PbPb/forest_miniAOD_ParticleTransformer_run3_DATA.py
@@ -47,50 +47,17 @@ process.HiForestInfo.GlobalTagLabel = process.GlobalTag.globaltag
 ###############################################################################
 
 # Define centrality binning
-#process.load("RecoHI.HiCentralityAlgos.CentralityBin_cfi")
-process.centralityBin = cms.EDProducer('HICentralityBinProducer')
+process.load("RecoHI.HiCentralityAlgos.CentralityBin_cfi")
 process.centralityBin.Centrality = cms.InputTag("hiCentrality")
 process.centralityBin.centralityVariable = cms.string("HFtowers")
-process.centralityBin.table = cms.vdouble(
-    0, 12.6832, 13.6619, 14.6105, 15.5607, 16.5373, 17.5537,
-    18.5564, 19.5798, 20.6429, 21.7309, 22.8571, 24.0171, 25.2076,
-    26.4617, 27.7636, 29.1224, 30.4889, 31.9073, 33.4092, 34.9668,
-    36.5944, 38.2687, 40.0331, 41.8499, 43.7441, 45.7609, 47.8702,
-    50.0602, 52.3383, 54.6888, 57.1595, 59.7371, 62.4344, 65.2259,
-    68.1811, 71.2326, 74.432, 77.7095, 81.1494, 84.6287, 88.3611,
-    92.1763, 96.1806, 100.254, 104.009, 108.08, 112.248, 116.552,
-    121.036, 125.702, 130.565, 135.555, 140.749, 146.153, 151.738,
-    157.522, 163.448, 169.658, 176.095, 182.65, 189.422, 196.538,
-    203.811, 211.453, 219.301, 227.39, 235.685, 244.613, 253.483,
-    262.543, 272.061, 281.896, 292.038, 302.561, 313.272, 324.162,
-    335.481, 347.218, 359.452, 371.843, 384.608, 397.604, 410.844,
-    424.509, 438.49, 452.835, 467.758, 483.136, 498.526, 514.487,
-    531.071, 548.156, 565.561, 582.963, 601.208, 619.383, 638.217,
-    657.284, 677.267, 698.087, 718.534, 739.375, 760.939, 782.844,
-    805.343, 828.273, 851.321, 875.144, 899.379, 924.104, 949.38,
-    975.348, 1001.85, 1028.69, 1056.28, 1084.29, 1112.53, 1142.33,
-    1172.64, 1202.7, 1233.39, 1264.6, 1297.06, 1329.3, 1363.64,
-    1397.15, 1431.01, 1465.92, 1500.78, 1536.97, 1573.28, 1610.11,
-    1648.02, 1686.4, 1724.65, 1764.5, 1804.89, 1845.34, 1886.97,
-    1929.29, 1972, 2016.06, 2060.73, 2107.12, 2153.1, 2199.57,
-    2247.47, 2295.09, 2344.12, 2394.58, 2444.16, 2495.4, 2548.6,
-    2601.83, 2655.17, 2711.34, 2767.64, 2825.43, 2882.83, 2941.54,
-    3001.24, 3062.53, 3124.61, 3185.75, 3250.33, 3316.9, 3382.93,
-    3450.68, 3519.6, 3591.77, 3662.97, 3736.13, 3808.08, 3883.4,
-    3960, 4036.6, 4117.4, 4198.46, 4282.42, 4367.15, 4454.81,
-    4544.46, 4635.13, 4727.1, 4820.67, 4917.54, 5017.95, 5117.8,
-    5224.09, 5331.97, 5441.61, 5554.64, 5672.17, 5791.71, 5914.83,
-    6041.3, 6177.19, 6324.17, 6496.65, 7289.22
-)
-#process.GlobalTag.snapshotTime = cms.string("9999-12-31 23:59:59.000")
-#process.GlobalTag.toGet.extend([
-#    cms.PSet(
-#        record = cms.string("HeavyIonRcd"),
-#        tag = cms.string("CentralityTable_HFtowers200_DataPbPb_periHYDJETshape_run3v140x01_offline_Nominal"),
-#        connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
-#        label = cms.untracked.string("HFtowers")
-#    ),
-#])
+process.GlobalTag.toGet.extend([
+    cms.PSet(
+        record = cms.string("HeavyIonRcd"),
+        tag = cms.string("CentralityTable_HFtowers200_DataPbPb_periHYDJETshape_run3v161x1_offline"),
+        connect = cms.string("sqlite_file:CentralityTable_HFtowers200_DataPbPb2026_periHYDJETshape_run3v161x1_offline_Nominal.db"),
+        label = cms.untracked.string("HFtowers")
+    ),
+])
 
 ###############################################################################
 
